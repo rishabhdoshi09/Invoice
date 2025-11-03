fd1ce86850f95f78745e51a6d244644b575e83eb
Emergent Agent <emergent@agent.com>
2025-11-03 19:33:26 +0000

    auto-commit for 2642e7aa-67a5-439c-b3f9-0955dcc9fd7e

--- backend/src/routes/tallyExport.js
+++ backend/src/routes/tallyExport.js
@@ -3,11 +3,13 @@ const Controller = require('../controller');
 module.exports = (router) => {
     router
         .route('/export/tally/sales')
-        .get(Controller.tallyExport.exportSales);
+        .get(Controller.tallyExport.exportSales)
+        .post(Controller.tallyExport.exportSelectedSales);
 
     router
         .route('/export/tally/purchases')
-        .get(Controller.tallyExport.exportPurchases);
+        .get(Controller.tallyExport.exportPurchases)
+        .post(Controller.tallyExport.exportSelectedPurchases);
 
     router
         .route('/export/tally/payments')
