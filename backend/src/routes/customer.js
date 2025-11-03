244c9335764fcfeb0257b9cd98d6d61a577b5ec2
Emergent Agent <emergent@agent.com>
2025-11-03 19:08:55 +0000

    auto-commit for 25ff811c-f40a-465b-bb18-d4d9dc8ceb73

--- backend/src/routes/customer.js
+++ backend/src/routes/customer.js
@@ -1,10 +1,15 @@
-const Router = require("express").Router();
-const Controller = require("../controller");
+const Controller = require('../controller');
 
-Router.post("/", Controller.customer.createCustomer);
-Router.get("/", Controller.customer.listCustomers);
-Router.get("/:customerId", Controller.customer.getCustomer);
-Router.put("/:customerId", Controller.customer.updateCustomer);
-Router.delete("/:customerId", Controller.customer.deleteCustomer);
+module.exports = (router) => {
+    router
+        .route('/customers')
+        .post(Controller.customer.createCustomer)
+        .get(Controller.customer.listCustomers)
 
-module.exports = Router;
+    router
+        .route('/customers/:customerId')
+        .get(Controller.customer.getCustomer)
+        .put(Controller.customer.updateCustomer)
+        .delete(Controller.customer.deleteCustomer)
+};
+
